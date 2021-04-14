3d94d6aa4f81157a0d58f5f0ec1ae73af38d2df0
Ananda Muhammad Zahir <anandazahir12@gmail.com>
2021-04-14 22:09:00 +0700

    tampah PHP

---
+++ PHP/percobaan2.php
@@ -0,0 +1,7 @@
+<?php
+    $makanan[0]="bakso";
+    $makanan[1]="sate";
+    $makanan[2]="tahu";
+    $makanan[3]="tempe";
+    echo "Saya suka makan ".$makanan[3]." dan " .$makanan[1].". Semuanya mak nyuuus";
+?>
